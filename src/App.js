c38d819e134b7a1f0de2f0d95803e165565a7218
PRakhar <srivastavaprakhar3010@gmail.com>
2024-06-06 12:11:26 +0530

    created the task of changing the background color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [color, setColor] = useState('rgb(255,255,255)');
+
+  useEffect(() => {
+    const btn = document.querySelector('#btn1');
+    const handleClick = () => {
+      const b = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const r = Math.floor(Math.random() * 256);
+
+      const newColor = `rgb(${r},${g},${b})`;
+      setColor(newColor);
+      document.body.style.backgroundColor = newColor;
+    };
+
+    btn.addEventListener('click', handleClick);
+
+    return () => {
+      btn.removeEventListener('click', handleClick);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Current color: {color}</p>
+      <button id='btn1'>Click me</button>
     </div>
   );
 }
